d6732bd63feb97fc30647a057519aaed11a26a92
Logan910115 <denny610230@gmail.com>
2024-06-20 15:04:29 +0800

    commit

---
+++ HW6/.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("sidebar").classList.add("open");
+    document.getElementById("main").classList.add("open");
+}
+
+function closeNav() {
+    document.getElementById("sidebar").classList.remove("open");
+    document.getElementById("main").classList.remove("open");
+}
+//不太會 此為詢問chatgpt
